f3a3f2fc0f89eef4445d7e162d3fc9f6f081a841
FarrowLS <peterwfarrow@yahoo.com>
2016-03-29 15:22:22 -0400

    Created directory structure for API
    The API has been given a MVC like structure in its own directory: app_api. Routes and
    views have been removed from the top level of the application to keep the structure clear
    and compartmentalized.

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./app_api/routes/index');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_api', 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
@@ -24,7 +23,6 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
